f5c7a8b04a76547e5925c0d01ef424b188cf37d1
heatondev <35467354+heatondev@users.noreply.github.com>
2021-10-14 13:20:36 -0600

    Added 2D scene and simple input based movement

--- MMP_Tutorials/Assets/Scripts/SimpleMove.cs
+++ MMP_Tutorials/Assets/Scripts/SimpleMove.cs
@@ -4,15 +4,32 @@ using UnityEngine;
 
 public class SimpleMove : MonoBehaviour
 {
+    Vector3 moveDirection;
+    float moveX;
+    float moveZ;
+
+    public float speed;
+
+    CharacterController cc;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        cc = GetComponent<CharacterController>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        moveX = Input.GetAxisRaw("Horizontal");
+        Debug.Log(moveX);
+        moveZ = Input.GetAxisRaw("Vertical");
+
+        moveDirection = new Vector3(moveX, 0.0f, moveZ).normalized;
+    }
+
+    private void FixedUpdate()
+    {
+        cc.Move(moveDirection * Time.deltaTime * speed);
     }
 }
