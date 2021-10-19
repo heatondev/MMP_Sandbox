a9edc474b9691d65ffeeb1405e99bb4884108fe6
Tyler <heatondev@gmail.com>
2021-10-19 12:16:32 -0600

    Demo stuff

---
+++ MMP_Tutorials/Assets/PlayerMoveSimple3D.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMoveSimple3D : MonoBehaviour
+{
+    CharacterController character;
+
+    float moveX;
+    float moveZ;
+
+    Vector3 movement;
+
+    public float speed = 5;
+    public float gravityValue;
+    
+    void Start()
+    {
+        character = GetComponent<CharacterController>();
+    }
+
+    
+    void Update()
+    {
+        moveX = Input.GetAxisRaw("Horizontal");
+        // -1 for left, +1 for right
+        moveZ = Input.GetAxisRaw("Vertical");
+
+        movement = new Vector3(moveX, 0.0f, moveZ);
+    }
+    void handlegravity()
+    {
+        if (character.isGrounded)
+        {
+            float groundgravity = -.05f;
+            movement.y = groundgravity;
+        }
+        else
+        {
+
+            movement.y = gravityValue;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        character.Move(movement * speed*Time.deltaTime);
+    }
+}
